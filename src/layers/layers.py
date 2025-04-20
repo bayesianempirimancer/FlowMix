0d978f46038a5a749956a5ba582c6099764149d8
Jeff Beck <bayesian.empirimancer@gmail.com>
2025-04-20 16:00:36 -0400

    Update layers.py
    initial bug fixes for Parallel Routines

--- src/layers/layers.py
+++ src/layers/layers.py
@@ -4,8 +4,8 @@ import jax.numpy as jnp
 import jax.random as jr
     
 class ParallelDense(nn.Module):
-    num_units: int  # Number of units in each dense layer
     num_par: int  # Number of parallel transformations
+    num_units: int  # Number of units in each dense layer
 
     @nn.compact
     def __call__(self, inputs):
@@ -27,13 +27,13 @@ class ParallelDense(nn.Module):
             raise ValueError(f"Input shape {inputs.shape} does broadcast to {self.block_size}")
 
 class ParallelMLP(nn.Module):
-    block_size: int
+    num_par: int
     features: tuple
 
     @nn.compact
     def __call__(self, x):        
         for i, feature in enumerate(self.features):
-            x = ParallelDense(feature, self.block_size)(x)
+            x = ParallelDense(self.num_par, feature)(x)
             if i != len(self.features) - 1:
                 x = nn.relu(x)
         return x
@@ -50,10 +50,9 @@ class ParallelRealNVPNode(nn.Module):
         self.static_mask_mat = jnp.eye(dim)[mask_dims]
 
         mask_dims = jnp.where(self.mask)
-        mask_dim = len(mask_dims)
+        mask_dim = len(mask_dims[0])
         self.update_mask_mat = jnp.eye(dim)[mask_dims]
 
-        # Initialize MLPs for computing s and t
         self.s_mlp = ParallelMLP(self.num_par, self.mlp_features + (mask_dim,))
         self.t_mlp = ParallelMLP(self.num_par, self.mlp_features + (mask_dim,))
 
@@ -69,46 +68,65 @@ class ParallelRealNVPNode(nn.Module):
 
         s = self.s_mlp(x_static)
         t = self.t_mlp(x_static)
-        log_det_jac += jnp.sum(s)
-                
+        log_det_jac += jnp.sum(s,-1)
+
         x_to_update = x_to_update * (jnp.exp(s)-1.0) + t
         x = jnp.sum(x_to_update[...,:,None]*self.update_mask_mat,-2) + x
 
         return x, log_det_jac
 
+import numpyro.distributions as dists
 
 class ParallelRealNVP(nn.Module):
     num_par: int  # Number of parallel transformations
+    dim: int  # Number of dimensions
+    num_nodes: int  # Number of NVP nodes
     mlp_features: tuple  # hidden layer sizes for the MLPs that compute s and t
-    masks: int  # Number of nodes in the RealNVP network
+    mask_seed: int = 88
+
+#    masks: jnp.ndarray  # masks.shape[-2] = number of NVP nodes, 
+#                        # masks.shape[-1] = number of dimensions
 
     # Note that this assumes that x is of shape (batch_size, num_par, dim) or (batch_size, 1, dim)
-    def setup(self):
-        num_nodes = self.masks.shape[0]
+    def setup(self):        
+        key = jr.PRNGKey(self.mask_seed)
         nodes = []
-        for i in range(num_nodes):
-            mask = self.masks[i]
-            if mask.sum() == 0:
-                raise ValueError(f"Mask {mask} has no dimensions to transform.")
+        for i in range(self.num_nodes):
+            key, subkey1, subkey2 = jr.split(key,3)
+            indices = jr.permutation(subkey2, jnp.arange(self.dim))[:self.dim//2 + jr.bernoulli(subkey1, 0.5)]
+            mask = jnp.zeros(self.dim, dtype=bool)
+            for j in range(len(indices)):
+                mask = mask.at[indices[j]].set(True)    
             nodes.append(ParallelRealNVPNode(self.num_par, self.mlp_features, mask))
         self.nodes = nodes
+        self.prior = dists.MultivariateNormal(jnp.zeros(self.dim), jnp.eye(self.dim))
         
     def __call__(self, x):   # assumes x has shape (batch, num_par, dim) or (bathc, 1, dim)
         inputs = (x, 0.0)
         for node in self.nodes:
             inputs = node(inputs)
-        
-# self = ParallelRealNVPNode(4, (5,5), jnp.array([False, True, False]))
-# x = jnp.ones((5, 1, 3))
-# params = self.init(jr.PRNGKey(0), (x, 0.0))
-# y = self.apply(params, (x, 0.0))
+
+        y, log_det_jac = inputs
+        return log_det_jac + self.prior.log_prob(y)
+
+class MixRealNVP(nn.Module):
+    mix_dim: int #Also equal to numbber of parallel transformations in Parallel* stuff
+    mlp_features: tuple  # hidden layer sizes for the MLPs that compute s and t
+
+    def setup(self):
+        pass
+
+    def __call__(self, x):   # assumes x has shape (batch, num_par, dim) or (bathc, 1, dim)
+        pass
 
 
-# masks = jnp.concatenate((1-jnp.eye(3), jnp.eye(3)), axis=0)
-# masks = jnp.concatenate((masks, masks), axis=0)>0.5
+self = ParallelRealNVPNode(4, (5,5), jnp.array([False, True, False]))
+x = jnp.ones((5, 1, 3))
+params = self.init(jr.PRNGKey(0), (x, 0.0))
+y = self.apply(params, (x, 0.0))
 
-# self = ParallelRealNVP(4, (5,5), masks)
-# x = jnp.ones((5, 1, 3))
-# params = self.init(jr.PRNGKey(0), x)
-# self.apply(params, x)
+self = ParallelRealNVP(4, 3, 10, (5,5))
+x = jnp.ones((5, 1, 3))
+params = self.init(jr.PRNGKey(1), x)
+logp = self.apply(params, x)
 
